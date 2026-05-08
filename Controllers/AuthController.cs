524bf276df8be90ad3020e6ad6885118315a5b07
Matheus-A-Guedes936 <matheusguedes936@gmail.com>
2026-05-08 15:06:48 -0300

    feat(auth): configura JWT Bearer e integração com Scalar API

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using GerenciadorDeVideos_API.DTOs.Usuarios;
+using GerenciadorDeVideos_API.Interface.IServices;
+using GerenciadorDeVideos_API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorDeVideos_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public AuthController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+        [HttpPost("Login")]
+        public async Task<ActionResult<ResponseModel<string>>> Login(UsuarioLoginDto loginDto)
+        {
+            var response = await _usuarioService.Login(loginDto);
+            if (!response.Status)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
